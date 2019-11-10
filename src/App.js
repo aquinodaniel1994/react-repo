0203dbfd18d0d823e734e6afaf7083a4979a0647
Daniel Aquino <daquino@pic-informatica.it>
2019-11-10 16:12:37 +0100

    adding component style and inline style

--- src/App.js
+++ src/App.js
@@ -19,11 +19,19 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: "white", 
+      border: "1px solid blue", 
+      borderRadius: "15px",
+      padding: "20px",
+      marginTop: "15px",
+      cursor: "pointer"
+    }
     return (
       <div className="App">
-        <button onClick={this.changeName.bind(this, "bottone")}>Change name</button>
+        <button style={style} onClick={this.changeName.bind(this, "bottone")}>Change name</button>
         <Person click={this.changeName.bind(this,"muller")} name={this.state.person[0].name} age={Math.floor(Math.random() * 25)} />
-        <Person name={this.state.person[1].name} age={Math.floor(Math.random() * 25)}> props children </Person>
+        <Person name={this.state.person[1].name} age={15}> props children </Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Person.css';
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>i'm {props.name} and i have {props.age}</p>
             <span>{props.children}</span>
         </div>
