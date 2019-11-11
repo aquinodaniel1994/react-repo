a7644d9d57bee7c803edd4648ea7e6bf450bc1c9
Daniel Aquino <daquino@pic-informatica.it>
2019-11-11 12:22:50 +0100

    toggle a component (show and hide)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    name: "ciccio",
+    showPerson: false
+  }
+
+  togglePerson = () => {
+    this.setState({showPerson: !this.state.showPerson })
+  }
+
+
+
+  render() {
+    let person = null;
+    if (this.state.showPerson) {
+      person = (
+        <div>
+          <Person name={this.state.name} />
+        </div>
+      )
+    }
+
+    return (
+      <div className="App" >
+        <button type="button" onClick={this.togglePerson}>Toggle Person</button>
+        {person}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+const Person = (props) => {
+    return (
+        <p>{props.name}</p>
+    );
+}
+
+
+export default Person;
