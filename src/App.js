1f64d4eea93c9f5e334c746ce5ae59b1c3f2ddc6
Daniel Aquino <daquino@pic-informatica.it>
2019-11-18 12:54:15 +0100

    using propsTypes anche check error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
+const Title = (props) => {
+  return (<h1>Title: {props.text}</h1>)
+}
+Title.propTypes = {
+  text(props, propName, component) {
+    if (!(propName in props)) {
+      return new Error(`missing proprs: ${propName}`)
+    }
+    if (props[propName].length < 10) {
+      return new Error(`${propName} is short`)
+    }
+  }
+}
+
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+        <Title  text="test"/>
+        <Title text="12345678901" />
+        <Title />
       </div>
     );
   }
